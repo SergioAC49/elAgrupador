6eae96f2ed1e9ef1d39a44319e80e2c959944472
Victor Gonzalez <victorggep@gmail.com>
2020-07-29 18:47:49 +0200

    First commit

---
+++ scrapers/scrapers.py
@@ -0,0 +1,208 @@
+import datetime
+import locale
+import requests
+
+from bs4 import BeautifulSoup
+
+
+class NewspaperScraper:
+
+    def __init__(self, url):
+        """
+        Read the URL and create the BeutifulSoup object
+        :param url: URL of the news
+        """
+        self.url = url
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+        }
+        req = requests.get(url, headers)
+        self.soup = BeautifulSoup(req.content, 'html.parser')
+
+    def get_title(self):
+        """
+        :return: String containing the title
+        """
+        raise Exception("Not Implemented")
+
+    def get_subtitles(self):
+        """
+        :return: List of strings with all the subtitles
+        """
+        raise Exception("Not Implemented")
+
+    def get_text(self):
+        """
+        :return: String with all the text
+        """
+        raise Exception("Not Implemented")
+
+    def get_authors(self):
+        """
+        :return: List of strings with all the author
+        """
+        raise Exception("Not Implemented")
+
+    def get_date(self):
+        """
+        :return: Datetime of publication date
+        """
+        raise Exception("Not Implemented")
+
+
+class ElPeriodicoScraper(NewspaperScraper):
+
+    def get_title(self):
+        return self.soup.find("h1", "title").get_text()
+
+    def get_subtitles(self):
+        subtitles = self.soup.find("div", "subtitle").find_all("h2")
+        return [h2.get_text() for h2 in subtitles]
+
+    def get_text(self):
+        all_p = self.soup.find("div", "ep-detail-body").find_all("p")
+
+        filtered_p = []
+        for p in all_p:
+            # Get all <p> that are text (not titles and subtitles)
+            if not (p.has_attr("class") and ("subtitle" in p['class'] or "title" in p['class'])):
+                filtered_p.append(p.get_text())
+
+        return "\n".join(filtered_p)
+
+    def get_authors(self):
+        return [self.soup.find("span", "author-link").get_text()]
+
+    def get_date(self):
+        str_date = self.soup.find("span", "dateModified").get_text()
+        date = datetime.datetime.strptime(str_date, "%d/%m/%Y - %H:%M")
+        return date
+
+
+class LaVanguardiaScraper(NewspaperScraper):
+
+    def get_title(self):
+        return self.soup.find("h1", "d-title__txt").get_text()
+
+    def get_subtitles(self):
+        subtitles = self.soup.find("div", "d-subtitle__list").find_all("h2")
+        return [h2.get_text() for h2 in subtitles]
+
+    def get_text(self):
+        all_p = self.soup.find("div", "story-leaf-txt-p").find_all("p")
+        p_text = [p.get_text() for p in all_p]
+        return "\n".join(p_text)
+
+    def get_authors(self):
+        return [self.soup.find("a", "d-signature__name").find("span").get_text()]
+
+    def get_date(self):
+        str_date = self.soup.find("time", "d-signature__time")['datetime']
+        date = datetime.datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%S%z")
+        return date
+
+
+class ElPaisScraper(NewspaperScraper):
+
+    def get_title(self):
+        return self.soup.find("h1", "a_t").get_text()
+
+    def get_subtitles(self):
+        subtitle = self.soup.find("h2", "a_st").get_text()
+        return [subtitle]
+
+    def get_text(self):
+        all_p = self.soup.find("div", "a_b").find_all("p")
+        p_text = [p.get_text() for p in all_p]
+        return "\n".join(p_text)
+
+    def get_authors(self):
+        all_a = self.soup.find_all("a", "a_aut_n")
+        authors = [a.get_text() for a in all_a]
+        return authors
+
+    def get_date(self):
+        str_date = self.soup.find("a", "a_ti").get_text()
+        locale.setlocale(locale.LC_ALL, 'es_ES.utf8')
+        date = datetime.datetime.strptime(str_date[:6] + "." + str_date[6:], "%d %b %Y - %H:%M %Z")
+        return date
+
+
+class ElMundoScraper(NewspaperScraper):
+
+    def get_title(self):
+        return self.soup.find("h1", "ue-c-article__headline").get_text()
+
+    def get_subtitles(self):
+        subtitle = self.soup.find("p", "ue-c-article__standfirst").get_text()
+        return [subtitle]
+
+    def get_text(self):
+        all_p = self.soup.find("div", "ue-c-article__body").find_all("p")
+        p_text = [p.get_text() for p in all_p]
+        return "\n".join(p_text)
+
+    def get_authors(self):
+        all_a = self.soup.find_all("div", "ue-c-article__byline-name")
+        authors = [a.get_text() for a in all_a]
+        return authors
+
+    def get_date(self):
+        str_date = self.soup.find("div", "ue-c-article__publishdate").find("time")['datetime']
+        date = datetime.datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%SZ")
+        return date
+
+
+class ABCScraper(NewspaperScraper):
+
+    def get_title(self):
+        return self.soup.find("span", "titular").get_text()
+
+    def get_subtitles(self):
+        subtitle = self.soup.find("h2", "subtitulo").get_text()
+        return [subtitle]
+
+    def get_text(self):
+        all_p = self.soup.find("span", "cuerpo-texto").find_all("p")
+        p_text = [p.get_text() for p in all_p]
+        return "\n".join(p_text)
+
+    def get_authors(self):
+        all_a = self.soup.find("footer", "autores").find_all("a", "nombre")
+        authors = [a.get_text() for a in all_a]
+        return authors
+
+    def get_date(self):
+        str_date = self.soup.find("span", "fecha").find("time")['datetime']
+        date = datetime.datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%SZ")
+        return date
+
+
+class ElDiarioScraper(NewspaperScraper):
+
+    def get_title(self):
+        return self.soup.find("h1", "title").get_text()
+
+    def get_subtitles(self):
+        all_li = self.soup.find("div", "news-header").find_all("li")
+        return [li.get_text() for li in all_li]
+
+    def get_text(self):
+        all_p = self.soup.find_all("p", "article-text")
+        p_text = [p.get_text() for p in all_p]
+        return "\n".join(p_text)
+
+    def get_authors(self):
+        all_a = self.soup.find("p", "authors").find_all("a")
+        authors = [a.get_text() for a in all_a]
+        return authors
+
+    def get_date(self):
+        str_date = self.soup.find("footer", "news-date").find("div", "date").get_text()
+        locale.setlocale(locale.LC_ALL, 'es_ES.utf8')
+        date = datetime.datetime.strptime(str_date, "%d de %B de %Y - %H:%M h")
+        return date
---
+++ scraping_one_news.py
@@ -0,0 +1,51 @@
+import argparse
+from scrapers import *
+
+
+def save_data(scraper):
+    print("Title:")
+    print(scraper.get_title())
+    print("Subtitles:")
+    print(scraper.get_subtitles())
+    print("Text:")
+    print(scraper.get_text())
+    print("Author:")
+    print(scraper.get_authors())
+    print("Date:")
+    print(scraper.get_date())
+
+
+def get_scraper(newspaper):
+    scraper_switcher = {
+        "elperiodico": ElPeriodicoScraper,
+        "lavanguardia": LaVanguardiaScraper,
+        "elpais": ElPaisScraper,
+        "elmundo": ElMundoScraper,
+        "abc": ABCScraper,
+        "eldiario": ElDiarioScraper
+    }
+    return scraper_switcher.get(newspaper)
+
+
+if __name__ == '__main__':
+    """
+    Call the script as: 
+    python3 scraping_one_news.py --url {url} --newspaper {newspaper}
+    
+    Example URLs:
+    'elperiodico': https://www.elperiodico.com/es/sanidad/20200706/la-incidencia-del-virus-en-segria-es-20-veces-mayor-que-en-catalunya-8028249
+    'lavanguardia': https://www.lavanguardia.com/politica/20200707/482180289185/reyes-catalunya-figueres-barcelona-murcia.html
+    'elpais': https://elpais.com/espana/madrid/2020-07-07/madrid-recurre-la-resolucion-del-ministerio-de-sanidad-sobre-los-controles-en-barajas-hay-que-exigir-test-en-origen-48-horas-antes-de-viajar.html
+    'elmundo': https://www.elmundo.es/espana/2020/07/07/5f0452b9fc6c83977f8b462a.html
+    'abc': https://www.abc.es/tecnologia/moviles/aplicaciones/abci-estados-unidos-extiende-lucha-contra-china-estudia-resringir-tiktok-y-otras-aplicaciones-chinas-202007071235_noticia.html#vca=rrss&vmc=abc-es&vso=tw&vli=cm-general&_tcode=ZTN2MnUx
+    'eldiario': https://www.eldiario.es/politica/iglesias-asegura-comparte-sanchez-estrategia-negociadora-presupuestos_1_6087609.html
+    """
+    # Read arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", required=True, help="url of the news")
+    parser.add_argument("--newspaper", required=True, help="name of the newspaper")
+    args = parser.parse_args()
+
+    # Get the correct scraper and save data
+    Scraper = get_scraper(args.newspaper.lower())
+    save_data(Scraper(args.url))
