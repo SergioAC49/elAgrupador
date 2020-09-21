7e38e80f8e383212bf9c9175fd8a34004eea3797
SergioAC49 <68963231+SergioAC49@users.noreply.github.com>
2020-09-21 23:48:00 +0200

    Update news_vectorizer.py

--- news_vectorizer.py
+++ news_vectorizer.py
@@ -1,22 +1,10 @@
-from bert_serving.server.helper import get_args_parser
-from bert_serving.server import BertServer
-from bert_serving.client import BertClient
-import tensorflow as tf
-
-def start_server(model_path):
-
-	args = get_args_parser().parse_args(['-model_dir', model_path,
-	                                     '-port', '5555',
-	                                     '-port_out', '5556',
-	                                     '-max_seq_len', 'NONE',
-	                                     '-mask_cls_sep',
-	                                     '-cpu'])
-	server = BertServer(args)
-	server.start()
+from sentence_transformers import SentenceTransformer, util
 
 def title_to_vector(news_title):
-
-  bc = BertClient()
-  title_vector = bc.encode([news_title])
-  
-  return title_vector
+	model = SentenceTransformer('distiluse-base-multilingual-cased')
+	
+	#Compute embeddings
+	embeddings = model.encode(test_dataset, convert_to_tensor=True)
+  	title_vector = embeddings[0]
+	
+	return title_vector
