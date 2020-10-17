8de1282349e4fdd78b9e9f1fbe04551b5931945f
Victor Gonzalez <victorggep@gmail.com>
2020-10-17 16:28:39 +0200

    Add newspaper to News node

--- api.py
+++ api.py
@@ -7,35 +7,47 @@ app = Flask(__name__)
 
 
 @app.route('/news/latest/')
-def hello_world():
+def latest_news():
+    # Create necessary arrays
+    news = []  # latest news
+    added_urls = []  # URLs in news array
+
+    # Make query
     n4_con = Neo4jConnector("bolt://localhost:50070", "neo4j", "pass.123")
-    main_pages = n4_con.get_main_page_news()
+    main_page = n4_con.get_main_page_news()
     n4_con.close()
-    added_urls = []
-    news = []
-    for n in main_pages:
-        if n['url'] not in added_urls:
-            tmp_news = [n['url']]
-            added_urls.append(n['url'])
-            for n2 in n['rel_news']:
-                if n2 not in added_urls:
-                    tmp_news.append(n2)
-                    added_urls.append(n2)
-            if len(tmp_news) > 2:
-                news.append(tmp_news)
+
+    # Process query
+    for related_news in main_page:
+        # Create list with the related news that hasn't been added before
+        list_news = []
+        list_urls = []
+        for n in related_news['rel_news']:
+            if n['url'] not in (added_urls or list_urls):
+                list_news.append(n)
+                list_urls.append(n['url'])
+
+        # If there are more than 2 related news, add to result
+        if len(list_news) > 2:
+            news.append(list_news)
+            added_urls.extend(list_urls)
     return json.dumps(news)
+
 
 @app.route('/news/search/')
 def search_news():
     return 'Not implemented!'
 
+
 @app.route('/news/category/')
 def category_news():
     return 'Not implemented!'
 
-@app.route('/news/<id>/')
-def get_news():
+
+@app.route('/news/<int:id>/')
+def get_news(id):
     return 'Not implemented!'
+
 
 @app.route('/news/<int:id>/similar/')
 def similar_news(id):
@@ -43,4 +55,4 @@ def similar_news(id):
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8088)
--- utils/neo4j_connector.py
+++ utils/neo4j_connector.py
@@ -35,7 +35,7 @@ class Neo4jConnector:
         news = tx.run(
             "MERGE (n:News { url: '"+url+"', title: '"+title.replace("'", '"')+"', "
             "vector: "+str(vector)+", picture_url: '"+picture_url + "', "
-            "timestamp: datetime('"+timestamp+"') })"
+            "timestamp: datetime('"+timestamp+"'), newspaper: '"+newspaper+"' })"
             "MERGE (np:Newspaper { name: '"+newspaper+"' })"
             "MERGE (n) -[:newspaper]-> (np)"
             "RETURN n.url AS url"
@@ -55,20 +55,21 @@ class Neo4jConnector:
     @staticmethod
     def _get_cos_similarities_and_return(tx, url, timestamp):
         result = tx.run(
-            "MATCH (p1:News {url:\""+url+"\"})"
-            "MATCH (p2:News)"
-            "WHERE p2.timestamp > datetime('"+timestamp+"')"
-            "RETURN p2.url AS url, gds.alpha.similarity.cosine(p1.vector, p2.vector) AS similarity"
+            "MATCH (p1:News {url:\""+url+"\"}) "
+            "MATCH (p2:News) "
+            "WHERE p2.timestamp > datetime('"+timestamp+"') AND p2.newspaper <> p1.newspaper "
+            "RETURN p2.url AS url, gds.alpha.similarity.cosine(p1.vector, p2.vector) AS similarity "
         )
         return [{"url": record["url"], "similarity": record["similarity"]} for record in result]
 
     @staticmethod
     def _get_main_page_news(tx, timestamp):
         result = tx.run(
-            "MATCH (n:News)-[r]->() WITH n, COUNT(r) AS n_rel, COLLECT(r) AS relations "
+            "MATCH (n:News)-[r:similar]->() WITH n, COUNT(r) AS n_rel, COLLECT(r) AS relations "
             "WHERE n.timestamp > datetime('"+timestamp+"')  AND n_rel > 2 "
-            "WITH n, n_rel, [x IN relations | endnode(x).url] AS rel_news "
+            "WITH n {.url, .title, .picture_url, .newspaper}, [x IN relations | endnode(x)] AS rel_news "
+            "WITH n, [x IN rel_news | x {.url, .title, .picture_url, .newspaper}] AS rel_news "
             "ORDER BY n.timestamp DESC "
-            "RETURN n.url AS news_url, n_rel, rel_news;"
+            "RETURN n, rel_news;"
         )
-        return [{"url": record[0], "rel_news": record[2]} for record in result]
+        return [{"rel_news": [record[0]]+record[1]} for record in result]
